b0a33e54185916905dea230ce58cfe89bb7e7a2e
Cheikh Mbengue <105053673+cheikhaa@users.noreply.github.com>
2023-11-14 13:11:32 +0000

    cheikh

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/booking', function () {
+    return view('booking');
+});
+
+Route::get('/Signin', function () {
+    return view('SignIn');
+});
+
