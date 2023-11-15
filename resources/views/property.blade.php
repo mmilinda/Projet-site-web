200d78c3c7e69cbe4d7c1d85e244b2a724ab476c
Moustapha Souane <msouane91@gmail.com>
2023-11-15 10:17:17 +0000

    final

--- resources/views/property.blade.php
+++ resources/views/property.blade.php
@@ -25,45 +25,14 @@
             .s{
                 padding-left: 5px;
             }
+            .property_projet{
+                margin-top:800px;
+            }
         </style>
 </head>
 
 <body>
-
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
-                aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Features</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Pricing</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                            aria-expanded="false">
-                            Dropdown link
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
-                        </ul>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-
+@include('header')
     <div class="container-fluid mt-2">
         <div class="row align-items-stretch">
             <div class="col-lg-7 col-md-12">
@@ -90,8 +59,8 @@
         </div>
     </div>
 
-    <div class="container">
-        <div class="row mt-5">
+    <div class="container property_projet">
+        <div class="row mt-5 ">
             <div class="col-lg-6">
                 <h1>Rhoncus suspendus</h1>
                 <div class="di">
@@ -190,7 +159,7 @@
             </div>
         </div>
 
-        <div class="row mt-5">
+        <div class="row  property_projet">
             <div class="text-center">
                 <h1>Amenities</h1>
             </div>
@@ -262,7 +231,7 @@
               
             </div>
         </div>
-        <div class="row mt-5">
+        <div class="row mt-5 ">
             <div class="text-center my-4">
                 <h1>Neighbourhood</h1>
             </div>
@@ -346,9 +315,12 @@
                 
             </div>
         </div>
+        <div class="container-fluid">
+    @include('footer')
     </div>
-
     
+    </div>
+   
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 <!-- Ajouter le script de l'API Google Maps -->
 <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap" async defer></script>
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,40 @@ use App\Http\Controllers\LandlordController;
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "web" middleware group. Make something great!
 |*/
+Route::get('/Guest_Detail', function () {
+    return view('/Guest_Detail');
+});
+
+Route::get('/checkoutpageA', function () {
+    return view('/checkoutpageA');
+});
+
+Route::get('/contact', function () {
+    return view('/contact');
+});
+Route::get('/landlord', function () {
+    return view('/projet.landlord');
+});
 
 
+Route::get('/blog', function () {
+    return view('blog');
+}); 
 
+Route::get('/booking', function () {
+    return view('booking');
+});
+
+Route::get('/Signin', function () {
+    return view('SignIn');
+});
+Route::get('/homepage', function () {
+    return view('homepage');
+});
+Route::get('/LogIn', function () {
+    return view('LogIn');
+     });
+Route::get('/property', function () {
+    return view('property');
+
+});
